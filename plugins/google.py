fbbefcf431c287885486ae771800a674e183ce55
Reisen <reisen@hellokitty.com>
2013-04-26 22:44:45 +0100

    Merge gis/google modules.

--- plugins/google.py
+++ plugins/google.py
@@ -2,6 +2,7 @@
     Search google for some useless shit no one cares about
 """
 from plugins.commands import command
+from random import randint
 
 from urllib.parse import quote_plus, unquote_plus
 from urllib.request import urlopen
@@ -29,3 +30,23 @@ def google(irc, nick, chan, msg, args):
     out = '%s -- \x02%s\x02: "%s"' % ( unquote_plus( query['url'], encoding='utf-8', errors='replace'), query['title'], query['content'] )
 
     return out.replace('<b>', '').replace('</b>', '')
+
+
+@command
+def image(irc, nick, chan, msg, args):
+    '''Google image search (returns any of the first 4 results)'''
+    url = 'https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=%s&safe=off'
+
+    if not msg:
+        return "Need something to search for."
+
+    # Searches are returned as JSON so we need to turn that into a dictionary
+    query = json.loads( urlopen( url % quote_plus( msg ) ).read().decode('UTF-8') )
+    if query['responseStatus'] != 200:
+        return query['responseStatus']
+
+    # Then break it down to only the data we need in this case it's any of the
+    # first 4 image results
+    query = query['responseData']['results'][randint(0,3)]['url']
+
+    return unquote_plus( query, encoding='utf-8', errors='replace' )
