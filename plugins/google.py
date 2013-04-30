106cd5601180a259556c4f891d1935c188ad48e0
Reisen <reisen@hellokitty.com>
2013-04-30 03:07:41 +0100

    Google and TV Fixes

--- plugins/google.py
+++ plugins/google.py
@@ -16,7 +16,7 @@ def google(irc, nick, chan, msg, args):
     print(nick, chan, msg, str(args))
     
     if not msg:
-        return "Need something to search for."
+        return 'Need something to search for.'
 
     # Searches are returned as JSON so we need to turn that into a dictionary
     query = json.loads( urlopen( url % quote_plus( msg ) ).read().decode('UTF-8') )
@@ -24,12 +24,15 @@ def google(irc, nick, chan, msg, args):
         return query['responseStatus']
 
     # Then break it down to only the data we need (which is the first result of our search)
-    query = query['responseData']['results'][0]
+    try:
+        query = query['responseData']['results'][0]
 
-    # We have to cut out the bold tags in the title for some reason
-    out = '%s -- \x02%s\x02: "%s"' % ( unquote_plus( query['url'], encoding='utf-8', errors='replace'), query['title'], query['content'] )
+        # We have to cut out the bold tags in the title for some reason
+        out = '%s -- \x02%s\x02: "%s"' % ( unquote_plus( query['url'], encoding='utf-8', errors='replace'), query['title'], query['content'] )
 
-    return out.replace('<b>', '').replace('</b>', '')
+        return out.replace('<b>', '').replace('</b>', '')
+    except:
+        return 'No results found.'
 
 
 @command
@@ -38,7 +41,7 @@ def image(irc, nick, chan, msg, args):
     url = 'https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=%s&safe=off'
 
     if not msg:
-        return "Need something to search for."
+        return 'Need something to search for.'
 
     # Searches are returned as JSON so we need to turn that into a dictionary
     query = json.loads( urlopen( url % quote_plus( msg ) ).read().decode('UTF-8') )
@@ -47,6 +50,9 @@ def image(irc, nick, chan, msg, args):
 
     # Then break it down to only the data we need in this case it's any of the
     # first 4 image results
-    query = query['responseData']['results'][randint(0,3)]['url']
+    try:
+        query = query['responseData']['results'][randint(0,3)]['url']
 
-    return unquote_plus( query, encoding='utf-8', errors='replace' )
+        return unquote_plus( query, encoding='utf-8', errors='replace' )
+    except:
+        return 'No results found'
