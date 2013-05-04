13fb0edeb3c03942f41e199dc6c80d58d8bb5242
Reisen <reisen@hellokitty.com>
2013-05-04 19:02:45 +0100

    Improve Core Utils

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,23 +1,18 @@
-# Collect Hooks Locally
-hooks = {'BRUH':[]}
+from collections import defaultdict
+
+# Collect Hooks Locally within the Plugins module. Other plugins and the IRC
+# core use this directly.
+hooks = defaultdict(lambda: [])
 
 def event(event_name):
     """Plugins decorate functions using this to hook events."""
     global hooks
 
     def hook(f):
-        # Events are not pre-populated in the dictionary. Events that have no
-        # hooks simply do not have keys. This keeps the dictionary small and
-        # allows the dispatcher to quikly decide whether or not to deal with an
-        # event.
-        if event_name not in hooks:
-            hooks[event_name] = []
-
         # Store the event hooked in the function, for access by other plugins.
         f.event = event_name
-
-        # Append this function to this list of handlers for this event.
         hooks[event_name].append(f)
+
         return f
 
     return hook
