1a1f6c56dfc1d4dffa843aea77701e6f2c8cf600
Reisen <reisen@hellokitty.com>
2013-05-05 23:47:20 +0100

    Remove old empty bruh directory.

--- plugins/channels.py
+++ plugins/channels.py
@@ -7,7 +7,7 @@ def setup_channels(irc, prefix, command, args):
     print(irc.config)
     if 'identify' in irc.config:
         irc.say('nickserv', 'identify {}'.format(irc.config['identify']))
-        sleep(1)
+        sleep(2)
 
     for channel in irc.config.get('channels', []):
         irc.join(channel)
