c0bb4dfc9848f32bddee308e90fac02e99c233b7
Reisen <reisen@hellokitty.com>
2013-05-24 09:40:13 +0100

    Finish Authentication Module

--- plugins/authentication.py
+++ plugins/authentication.py
@@ -4,8 +4,9 @@
     API plugin for example.
 """
 import hmac, struct, hashlib, os, base64
+from plugins import event
+from functools import wraps
 from plugins.commands import command
-
 
 def DF(password, salt, c, dkLen):
     def F(password, salt, c, i):
@@ -39,6 +40,13 @@ def DF(password, salt, c, dkLen):
     return result[:dkLen]
 
 
+@event('BRUH')
+def setup_auth(irc):
+    # List of currently authed users. This is the users full username prefix, such
+    # as: Bob!Bob@bobsicles.com
+    irc.auth_list = set()
+
+
 def setup_db(irc):
     irc.db.execute('''
         CREATE TABLE IF NOT EXISTS users (
@@ -57,9 +65,106 @@ def setup_db(irc):
     irc.db.commit()
 
 
+def authenticated(f):
+    @wraps(f)
+    def auth_wrapper(irc, nick, chan, msg, irc_args, *args, **kwargs):
+        # Only allow the command to continue if we're sure the user is already
+        # authenticated.
+        if irc_args[0] not in irc.auth_list:
+            return "You need to be logged in to use this command. Check .help login, or .help register"
+
+        # The database contains a list of Key/Value pairs related to a user, we
+        # populate these as a dictionary here, before the function call, and
+        # then provide it as an extra argument to the resulting function.
+        userid = irc.db.execute('SELECT * FROM users WHERE username = ?', (nick,)).fetchone()[0]
+        pairs  = dict(irc.db.execute('SELECT key, value FROM user_properties JOIN users ON users.id = user_id WHERE users.id = ?', (userid,)).fetchall())
+
+        result = f(irc, nick, chan, msg, irc_args, pairs, *args, **kwargs)
+
+        # Any changes the user made to the dictionary should be updated in the
+        # database so that future authed calls see the correct users state.
+        for key, value in pairs.items():
+            irc.db.execute('INSERT OR REPLACE INTO user_properties VALUES (?, ?, ?)', (
+                userid,
+                key,
+                value
+            ))
+        irc.db.commit()
+
+        return result
+
+    return auth_wrapper
+
+
+def do_authenticate(irc, nick, password):
+    # Check to make sure the user actually exists first. If not we can warn the
+    # user and leave early.
+    user = irc.db.execute('SELECT * FROM users WHERE username = ?', (nick,)).fetchone()
+    if not user:
+        return "Couldn't find your username, are you sure you registered? See .help register"
+
+    # Otherwise, we can go ahead and verify. Each user stores a 128 byte
+    # authentication token in the database consisting of a salt and their
+    # generated key.
+    data = base64.b64decode(user[2])
+    salt = data[:64]
+    key  = data[64:]
+
+    # Use the stored salt and their provided password to see if we get the same
+    # stored key, if so authentication was successful.
+    return DF(password.encode('UTF-8'), salt, 1000, 64) == key
+
+
+@command
+def logout(irc, nick, chan, msg, args):
+    """
+    Logout of the bot, ending an authenticated session.
+    .logout
+    """
+    if args[0] not in irc.auth_list:
+        return "You aren't logged in, so I can't really log you out."
+
+    irc.auth_list.remove(args[0])
+    return "You are now logged out."
+
+
+@command
+def login(irc, nick, chan, msg, args):
+    """
+    Authenticate with the bot.
+    .login <password>
+    """
+    setup_db(irc)
+
+    if args[0] in irc.auth_list:
+        return "You're already authenticated!"
+
+    # Catch common problems while authing and warn the user. First we stop them
+    # authing publicly.
+    if chan.startswith('#'):
+        return "You just tried to auth in a channel, publicly. You should try and auth again in a PM, then change your password."
+
+    # Also make sure they actually provided their password.
+    if not msg:
+        return "You need to provide a passworth when you authenticate."
+
+    try:
+        if do_authenticate(irc, nick, msg):
+            irc.auth_list.add(args[0])
+            return "You're now logged in."
+
+        return "Your password seems to be wrong, if you're sure it is right, there might be an error. Contact the bot owner."
+    except Exception as e:
+        print(e)
+        return "There was an error authenticating you. This was a bot error, please report it."
+
+
 @command
 def register(irc, nick, chan, msg, args):
-    """Register a new user with the bot."""
+    """
+    Register a new user with the bot.
+    .register <password>
+    """
     setup_db(irc)
 
     # Users may be dumb and register in a public channel, if they are the best
