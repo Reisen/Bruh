d7d73fb601e983896edc824cdf3238f03ea4d14f
Reisen <reisen@hellokitty.com>
2013-06-18 15:55:53 +0100

    Add clearer slightly more expansive optional debug messages.

--- plugins/ping.py
+++ plugins/ping.py
@@ -4,7 +4,7 @@ from plugins.commands import regex
 # Handles server PINGS
 @event('PING')
 def ping(irc, prefix, command, args):
-    irc.raw('PONG %s\r\n' % args[0])
+    irc.raw('PONG :{}\r\n'.format(args[0]))
 
 
 # Users can 'ping' bruh by using 'bruh!' in a message for it to respond to.
