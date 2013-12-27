746d5e67cb0919d1f62184f4127a1ef4e9cca3dc
Reisen <reisen@hellokitty.com>
2013-12-27 01:30:31 +0000

    Fix entities and referer in google module.

--- plugins/google.py
+++ plugins/google.py
@@ -5,31 +5,54 @@ from plugins.commands import command
 from random import randint
 
 from urllib.parse import quote_plus, unquote_plus
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
+from html.parser import HTMLParser
 import json
 
 @command(['g'])
 def google(irc, nick, chan, msg, args):
     '''Google web search'''
-    url = 'https://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=%s'
-
     if not msg:
         return 'Need something to search for.'
 
-    # Searches are returned as JSON so we need to turn that into a dictionary
-    query = json.loads( urlopen( url % quote_plus( msg ) ).read().decode('UTF-8') )
-    if query['responseStatus'] != 200:
-        return query['responseStatus']
+    # Build the request. The Referer is required because of googles ToS.
+    request = Request(
+        'https://ajax.googleapis.com/ajax/services/search/web?v=1.0&q={}'.format(quote_plus(msg)),
+        headers = {
+            'Referer': 'http://github.com/Reisen/Bruh',
+            'User-Agent': 'Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11'
+        }
+    )
 
-    # Then break it down to only the data we need (which is the first result of our search)
+    # Then break it down to only the data we need (which is the first result of
+    # our search.)
     try:
+        # Searches are returned as JSON so we need to turn that into a
+        # dictionary.
+        query = json.loads(urlopen(request).read().decode('UTF-8'))
+
+        # Check for any statuses that aren't a successful search.
+        if query['responseStatus'] != 200:
+            return "Google returned: {}".format(query['responseStatus'])
+
         query = query['responseData']['results'][0]
 
-        # We have to cut out the bold tags in the title for some reason
-        out = '%s -- \x02%s\x02: "%s"' % ( unquote_plus( query['url'], encoding='utf-8', errors='replace'), query['title'], query['content'] )
+        # Format the output for IRC.
+        out = '{} -- \x02{}\x02: "{}"'.format(
+            unquote_plus(query['url'], encoding='utf-8', errors='replace'),
+            query['title'],
+            query['content']
+        )
 
-        return out.replace('<b>', '').replace('</b>', '')
-    except:
+        # Strip anything useless, replace HTML entities, and output.
+        return HTMLParser().unescape(
+            out.replace('<b>', '')
+               .replace('</b>', '')
+               .replace('\n', '')
+               .strip()
+        )
+
+    except Exception as e:
         return 'No results found.'
 
 
