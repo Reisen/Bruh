d4146337f740b1993ecdff95b3072b8f1626c49f
Reisen <reisen@hellokitty.com>
2014-03-16 14:15:50 +0000

    Wikipedia and Formatting.

--- plugins/twitter.py
+++ plugins/twitter.py
@@ -9,25 +9,29 @@ from TwitterAPI import TwitterAPI
 @command
 def twitter(irc, nick, chan, msg, args):
     '''Retrieves requested users latest tweet'''
-    
+
     if 'twitter' in irc.config['plugins']:
-        consumer_key,consumer_secret,access_token_key,access_token_secret = irc.config['plugins']['twitter']['keys']
+        consumer_key, consumer_secret, access_token_key, access_token_secret = irc.config['plugins']['twitter']['keys']
     else:
-        return "Something went wrong"
-	
+        return "No 'keys' set for twitter API. Check your config."
+
     if not msg:
-        return "Need a user to search for"
-    
+        return "Need a user to search for."
+
     try:
         request = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
-        request = request.request('statuses/user_timeline',{'screen_name':msg,'count':'1'})
+        request = request.request('statuses/user_timeline', {'screen_name': msg, 'count': '1'})
 
         if request.status_code != 200:
             raise Exception("bad status code")
 
-        # removing the first and last characters because they are '[' and ']'
+        # Removing the first and last characters because they are '[' and ']'
         request = loads(request.text[1:-1])
-        return "{}  -  https://twitter.com/{}/status/{}".format(request['text'],request['user']['name'],request['id'])
-		
+        return "{}  -  https://twitter.com/{}/status/{}".format(
+            request['text'],
+            request['user']['name'],
+            request['id']
+        )
+
     except:
         return "Something went wrong"
