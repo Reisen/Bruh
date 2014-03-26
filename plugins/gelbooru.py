0b95f1f239e726978fb2e4ee17183dfbd74408ad
Reisen <reisen@morphism.org>
2014-03-26 17:31:22 +0000

    Fix comment retrieval.

--- plugins/gelbooru.py
+++ plugins/gelbooru.py
@@ -22,16 +22,22 @@ def gelbooru(irc, nick, chan, match, args):
     if not comment_cache or comment_count > 25 or time() - comment_delay >= 1200:
         # To limit the API hits, we cache the result and print from it until 25
         # comments have been printed or 20 minutes has passed.
-        print('API Hit: {} {} {}'.format(comment_cache is None, comment_count > 25, time() - comment_delay >= 1200))
         listing       = urlopen('http://gelbooru.com/index.php?page=dapi&s=comment&q=index', timeout = 7).read().decode('UTF-8')
         listing       = ElementTree.fromstring(listing)
-        for list_post in listing[:5]:
-            if list_post.attrib['id'] == comment_cache[0].attrib['id']:
-                return "Not many new comments have been posted yet. Wait a little while."
+
+        # Check to see if the newest comment is actually new, if it is, check
+        # if the last old comment is older than 5 new comments, otherwise make
+        # the user wait a while.
+        if comment_cache and listing[0].attrib['id'] != comment_cache[0].attrib['id']:
+            for list_post in listing[:5]:
+                if list_post.attrib['id'] == comment_cache[0].attrib['id']:
+                    return "Not many new comments have been posted yet. Wait a little while."
 
         comment_cache = listing
         comment_count = 0
         comment_delay = time()
+
+
 
     comment_count += 1
     return "{} - by {}. ({})".format(
