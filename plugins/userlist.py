57989ac74c63a0dd639e143ea8d2f470be6e1116
Reisen <reisen@morphism.org>
2014-03-26 15:29:42 +0000

    Track user modes in the userlist plugin.

--- plugins/userlist.py
+++ plugins/userlist.py
@@ -9,25 +9,43 @@ from plugins.commands import command
 from collections import defaultdict
 
 
+@command
+def usercount(irc, nick, chan, msg, args):
+    return "There are {} users in the channel currently.".format(len(irc.userlist[chan]))
+
+
 @event('BRUH')
 def prepare_userlist(irc):
     """Load a userlist dict into irc objects."""
     # Attach a userlist dict to each server. The keys are channels and the
     # values are sets of users.
-    irc.userlist = defaultdict(lambda: set())
+    irc.userlist = defaultdict(set)
+
+    # Same as previous, except with the addition of extra details such as the
+    # users mode. Keys are still channels with dictionaries for the users and
+    # their details.
+    irc.modelist = defaultdict(dict)
 
 
 @event('353')
 def parse_names(irc, prefix, command, args):
     """Parse RPLNAMREPLY messages in response to /NAMES or a join."""
-
-    # Begin adding users to the userlist from responses.
     for user in args[3].split(' '):
-        # Remove status characters.
-        if user[0] in '!+&%@~':
-            user = user[1:] 
+        # Remove status characters, and store them in a mode string. Sadly if
+        # users have more than one mode some servers are retarded and will send
+        # just @x instead of @+x. And so if a user who is @+x is deopped, we
+        # will not know they are voiced. This issue is present in irssi too so
+        # as far as I can tell It's a server-side fault that I really can't do
+        # anything about.
+        mode = set()
+        while user[0] in '!+&%@~':
+            mode.add(user[0])
+            user = user[1:]
 
         irc.userlist[args[2]].add(user)
+        irc.modelist[args[2]][user] = {
+            'mode': mode
+        }
 
 
 @event('JOIN')
@@ -70,3 +88,41 @@ def quit_userlist(irc, prefix, command, args):
     for channel, userlist in irc.userlist.items():
         if nick in userlist:
             userlist.remove(nick)
+
+
+@event('MODE')
+def mode_userlist(irc, prefix, command, args):
+    """Track changes in user modes in the channel."""
+    chan, mode, *nicks = args
+    print(chan, mode, nicks)
+    nicks.reverse()
+
+    mode_lookup = {
+        'v': '+',
+        'h': '%',
+        'o': '@',
+        'a': '&',
+        'q': '~'
+    }
+
+    # Mode strings can consist of multiple flags, toggles, and arguments. We
+    # just have to find the ones we care about though and make sure we track
+    # them correctly.
+    direction = False
+    while mode:
+        # Find out whether we're adding or removing flags, and eat the char.
+        # Direction should persist until a new +/- is seen.
+        if mode[0] in '+-':
+            direction = True if mode[0] == '+' else False
+            mode = mode[1:]
+
+        # Voice(+), Halfop(%), Op(@), Protected(&), Owner(~)
+        if mode[0] in 'vhoaq':
+            target = nicks.pop()
+            if direction:
+                irc.modelist[chan][target]['mode'].add(mode_lookup[mode[0]])
+            else:
+                irc.modelist[chan][target]['mode'].remove(mode_lookup[mode[0]])
+
+        # Eat the mode character and continue on.
+        mode = mode[1:]
