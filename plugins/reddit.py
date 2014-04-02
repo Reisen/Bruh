e7b3facff16ab604713c95ab983cf1a3b5be8168
Reisen <reisen@morphism.org>
2014-04-02 15:27:23 +0100

    Reddit Catch.

--- plugins/reddit.py
+++ plugins/reddit.py
@@ -13,11 +13,15 @@ def reddit_match(irc, nick, chan, match, args):
     # Only bother doing the request if the Youtube plugin exists. Otherwise
     # this whole function is a no-op.
     if 'youtube' in irc.plugins:
-        listing = urlopen('http://reddit.com/r/{}/duplicates/{}.json'.format(match.group(1), match.group(2)), timeout = 7)
-        listing = loads(listing.read().decode('utf-8'))
-        listing = listing[0]['data']['children'][0]['data']
+        try:
+            listing = urlopen('http://reddit.com/r/{}/duplicates/{}.json'.format(match.group(1), match.group(2)), timeout = 7)
+            listing = loads(listing.read().decode('utf-8'))
+            listing = listing[0]['data']['children'][0]['data']
 
-        # Parse Youtube URL's out of Reddit Links.
-        youtube_check = re.search(r'(?:youtube\..*?\?.*?v=([-_a-zA-Z0-9]+))', listing['url'])
-        if youtube_check:
-            return irc.plugins['youtube'].fetch_video(youtube_check.group(1), True)
+            # Parse Youtube URL's out of Reddit Links.
+            youtube_check = re.search(r'(?:youtube\..*?\?.*?v=([-_a-zA-Z0-9]+))', listing['url'])
+            if youtube_check:
+                return irc.plugins['youtube'].fetch_video(youtube_check.group(1), True)
+
+        except Exception as e:
+            pass
