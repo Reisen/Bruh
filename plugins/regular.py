744878f502eb4cb885e584e5f6c1494fbe716d4b
Reisen <reisen@morphism.org>
2015-02-25 18:23:34 +0000

    Regular expression matcher, no more lower case names.

---
+++ plugins/regular.py
@@ -0,0 +1,89 @@
+import re
+import random
+from bruh import command, r
+from plugins.userlist import userlist
+from drivers.walnut import Walnut
+
+
+def add_prefix(irc, arg):
+    try:
+        matcher, result = arg.split('<=>')
+        r.hset(irc.key + ':regular', matcher.strip(), result.strip())
+        return "Prefix successfully added."
+
+    except Exception as e:
+        return "Provide a proper translation. Syntax: from <=> to"
+
+
+def del_prefix(irc, arg):
+    matcher, process, result = try_prefix(
+        '{}:{}:regular'.format(irc.network, irc.channel),
+        arg
+    )
+
+    if matcher:
+        r.hdel(irc.key + ':regular', process)
+        return "The matcher matching your message has been deleted."
+
+    return "No matchers matched this string."
+
+
+def try_prefix(key, message):
+    for matchpair in r.hscan_iter(key):
+        result  = matchpair[1].decode('UTF-8')
+        process = matchpair[0].decode('UTF-8')
+        matcher = re.match(process, message, re.I)
+        if matcher:
+            return (matcher, process, result)
+
+    return (None, None, None)
+
+
+@Walnut.hook('PRIVMSG')
+def handle_regulars(message):
+    network = message.parent.frm
+    channel = message.args[0]
+    nick    = message.prefix.split('!')[0]
+
+    matcher, process, result = try_prefix(
+        '{}:{}:regular'.format(network, channel),
+        message.args[-1]
+    )
+
+    if matcher:
+        keyspace = {"\\" + str(k + 1): v for k, v in enumerate(matcher.groups())}
+        keyspace = dict(keyspace.items() | {
+            "channel": channel,
+            "nick": nick,
+            "rand1": lambda: random.sample(userlist[network][channel], 1)[0],
+            "rand2": lambda: random.sample(userlist[network][channel], 1)[0],
+            "rand3": lambda: random.sample(userlist[network][channel], 1)[0],
+            "rand4": lambda: random.sample(userlist[network][channel], 1)[0],
+            "rand5": lambda: random.sample(userlist[network][channel], 1)[0],
+            "rand6": lambda: random.sample(userlist[network][channel], 1)[0]
+        }.items())
+
+        for k, v in keyspace.items():
+            if callable(v):
+                keyspace[k] = keyspace[k]()
+
+        return "PRIVMSG {} :{}".format(
+            channel,
+            result.format(**keyspace)
+        )
+
+
+@command('re')
+def regular(irc):
+    if not irc.message:
+        return None
+
+    try:
+        cmd, *args = irc.message.split(' ', 1)
+        return {
+            "add": add_prefix,
+            "del": del_prefix
+        }[cmd](irc, *args)
+
+    except KeyError:
+        return "Unknown subcommand: {}".format(cmd)
