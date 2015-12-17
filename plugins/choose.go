26a07ba241329880552c52a374f6a9f20728c74a
hotdoginferno <hotdoginferno@users.noreply.github.com>
2015-12-17 14:30:12 -0800

    Added choose plugin
    Plugin to choose from a list of options

---
+++ plugins/choose.go
@@ -0,0 +1,22 @@
+//Choose randomly from a set of choices separated by commas.
+package choose
+
+import (
+        "math/rand"
+        "strings"
+)
+
+func choose(msg Message) []string {
+
+        tmp := strings.Split(msg.line, ",")
+
+        var choice int
+
+        if len(tmp) <= 1 {
+                choice = 0
+        } else {
+                choice = rand.Intn(len(tmp) - 1)
+        }
+
+        return []string{tmp[choice]}
+}
