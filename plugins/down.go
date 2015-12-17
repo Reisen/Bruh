22c7375e6326518adc64b9cfaa0a7957a2f75ac4
hotdoginferno <hotdoginferno@users.noreply.github.com>
2015-12-17 14:30:37 -0800

    Added down plugin
    Plugin to check if a website (http/s) is down

---
+++ plugins/down.go
@@ -0,0 +1,32 @@
+//Check if a website is down or not.
+package down
+
+import (
+        "net/http"
+        "strings"
+        "time"
+)
+
+func down(msg Message) []string {
+
+        //only checking http so we don't have to explicitly check for https
+        pre := "http"
+        var url string
+
+        if !strings.HasPrefix(tmp, pre) {
+                url = pre + "://"
+        }
+
+        timeout := time.Duration(7 * time.Second)
+        client := http.Client{Timeout: timeout}
+
+        url += msg.line
+
+        _, err := client.Get(url)
+
+        if err == nil {
+                return []string{"It's just you, " + url + " is up."}
+        } else {
+                return []string{"It's not just you, " + url + " seems to be down."}
+        }
+}
